8d696fe6b69adb09c7e8f76ccc5f25b8f91cebf1
GINTOAHC <chaotingchen10@gmail.com>
2025-08-31 21:14:09 +0800

    wip: refactor CLI with subcommands

---
+++ render/icons.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// Files
+static const char *icon_dir     = "\uF07B"; // 
+static const char *icon_file    = "\uF15B"; // 
+static const char *icon_exe     = "\uF489"; // 
+static const char *icon_image   = "\uF1C5"; // 
+static const char *icon_archive = "\uF1C6"; // 
+static const char *icon_symlink = "\uF0C1"; // 
+
+// Git
+static const char *icon_git_added    = "\uF055"; // 
+static const char *icon_git_modified = "\uF044"; // 
+static const char *icon_git_deleted  = "\uF057"; // 
+
+// Symbols
+static const char *icon_x       = "\uF00D"; // 
+static const char *icon_check   = "\uF00C"; // ✓
+static const char *icon_list    = "\uF03A"; // 
+static const char *icon_info    = "\uF129"; // 
+static const char *icon_warn    = "\uF071"; // 
+static const char *icon_error   = "\uF06A"; // 
+static const char *icon_package = "\uF1C6"; //  (duplicate of archive)
+static const char *icon_time    = "\uF017"; // 
+static const char *icon_star    = "\uF005"; // ★
+static const char *icon_arrow   = "\uF0AB"; // ➫
+static const char *icon_dot     = "\uF111"; // ●
