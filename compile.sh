10ce715a0ac3e7a39ffbaabcdb36c7fa404c0df1
GINTOAHC <chaotingchen10@gmail.com>
2025-12-28 23:50:52 +0800

    ci: compile script

--- compile.sh
+++ compile.sh
@@ -1,19 +1,24 @@
-gcc -c src/cmd/list/list.c -o obj/list.o
-gcc -c src/cmd/submit/submit.c -o obj/submit.o
-gcc -c src/cmd/stop/stop.c -o obj/stop.o
-gcc -c src/cmd/restart/restart.c -o obj/restart.o
-# gcc -c src/cmd/status/status.c -o build/status.o
+#!/bin/bash
+mkdir -p obj bin
 
-gcc -c core/daemonize.c -o obj/daemonize.o
-gcc -c core/state.c -o obj/state.o
+CC=${CC:-gcc}
 
-gcc -c render/colors.c -o obj/colors.o
-gcc -c render/icons.c -o obj/icons.o
+$CC -c src/cmd/list/list.c -o obj/list.o
+$CC -c src/cmd/submit/submit.c -o obj/submit.o
+$CC -c src/cmd/stop/stop.c -o obj/stop.o
+$CC -c src/cmd/restart/restart.c -o obj/restart.o
+# $CC -c src/cmd/status/status.c -o build/status.o
 
-gcc -c actions/list/list.c -o obj/action_list.o
-gcc -c actions/submit/submit.c -o obj/action_submit.o
-gcc -c actions/stop/stop.c -o obj/action_stop.o
+$CC -c core/daemonize.c -o obj/daemonize.o
+$CC -c core/state.c -o obj/state.o
 
-gcc -c src/main.c -o obj/main.o
+$CC -c render/colors.c -o obj/colors.o
+$CC -c render/icons.c -o obj/icons.o
 
-gcc obj/*.o -o bin/main
+$CC -c actions/list/list.c -o obj/action_list.o
+$CC -c actions/submit/submit.c -o obj/action_submit.o
+$CC -c actions/stop/stop.c -o obj/action_stop.o
+
+$CC -c src/main.c -o obj/main.o
+
+$CC obj/*.o -o bin/main
