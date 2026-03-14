c1665fc99665bd83f7d42cfff27ed25c2b46f8ff
Chao-Ting, Chen <chaotingchen10@gmail.com>
2026-03-14 14:29:22 +0800

    Add workdir & logdir to state file (#3)
    * feat: add workdir & logdir to state file
    
    * feat: inspect a process via list with --id flag
    
    * fix: some minor fix
    
    * fix: free memory after restarting process
    
    * refactor: remove legacy entry point

--- actions/list/list.h
+++ actions/list/list.h
@@ -1,3 +1,6 @@
 #pragma once
 
 int list(); /* List all processes, return -1 for error, 0 for success */
+
+/* List process by ID, return -1 for error, 0 for success */
+int list_by_id(const long id);
